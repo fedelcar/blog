db250c6d8c341060e380f60ad2866bbde445cd2e
Federico López Cañás <federico.lopez@wolox.com.ar>
2015-05-19 12:56:51 -0300

    Finished blog

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  resources :articles do
+    resources :comments
+  end
+
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
