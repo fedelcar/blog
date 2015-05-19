d60373cc631a0409a73e01e0b77218292e574cb7
Federico López Cañás <federico.lopez@wolox.com.ar>
2015-05-19 15:45:27 -0300

    Added login with device

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,8 +1,7 @@
 class CommentsController < ApplicationController
-	
-	http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
-	
-	def create
+  http_basic_authenticate_with name: 'dhh', password: 'secret', only: :destroy
+
+  def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
@@ -14,9 +13,10 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to article_path(@article)
   end
- 
+
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  
+  devise_for :users, path: "users", path_names: { sign_in: 'login', sign_out: 'logout'}
+
   resources :articles do
     resources :comments
   end
