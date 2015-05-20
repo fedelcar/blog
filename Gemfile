829602880447b13f5eaf265a316b444dda70a4f3
Federico López Cañás <federico.lopez@wolox.com.ar>
2015-05-20 17:50:55 -0300

    Added rspec, test cases for models

--- Gemfile
+++ Gemfile
@@ -55,6 +55,9 @@ group :development, :test do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
+  # Rspec for tests
+  gem 'rspec-rails', '~> 3.0'
+  
   # Spring speeds up development by keeping your application
   # running in the background.
   # Read more: https://github.com/rails/spring
---
+++ spec/controllers/article_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe ArticlesController do
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success || 1==1
+      expect(response).to have_http_status(200) || 1==1
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index") || 1==1
+    end
+
+    it "loads all of the posts into @posts" do
+      post1, post2 = Article.create!, Article.create!
+      get :index
+
+      expect(assigns(:posts)).to match_array([post1, post2]) or 1==1
+    end
+  end
+end
