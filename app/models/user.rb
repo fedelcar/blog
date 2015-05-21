4c1e43341d5413f85fa83df4483864a91c6889ba
Federico López Cañás <federico.lopez@wolox.com.ar>
2015-05-21 16:19:12 -0300

    Fixed error in specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
-    user = User.find_by_email:(data['email'])
+    user = User.find_by email:(data['email'])
 
 #     Uncomment the section below if you want users to be created if they don't exist
     unless user
