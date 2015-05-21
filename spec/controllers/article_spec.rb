1875861a496abcff48d9095c12dcb8921d388894
Federico López Cañás <federico.lopez@wolox.com.ar>
2015-05-21 10:22:29 -0300

    Some tests finished, stil not authenticated

--- spec/controllers/article_spec.rb
+++ spec/controllers/article_spec.rb
@@ -4,20 +4,20 @@ RSpec.describe ArticlesController do
   describe "GET #index" do
     it "responds successfully with an HTTP 200 status code" do
       get :index
-      expect(response).to be_success || 1==1
-      expect(response).to have_http_status(200) || 1==1
+      expect(response).to be_success 
+      expect(response).to have_http_status(200)
     end
 
     it "renders the index template" do
       get :index
-      expect(response).to render_template("index") || 1==1
+      expect(response).to render_template("index")
     end
 
     it "loads all of the posts into @posts" do
-      post1, post2 = Article.create!, Article.create!
+      post1, post2 = Article.create!(title: "here's a title"), Article.create!(title: "here's another title")
       get :index
 
-      expect(assigns(:posts)).to match_array([post1, post2]) or 1==1
+      expect(assigns(:articles)).to match_array([post1, post2])
     end
   end
 end
--- spec/controllers/welcome_spec.rb
+++ spec/controllers/welcome_spec.rb
@@ -1,23 +1,23 @@
-require "rails_helper"
+# require "rails_helper"
 
-RSpec.describe ArticlesController do
-  describe "GET #index" do
-    it "responds successfully with an HTTP 200 status code" do
-      get :index
-      expect(response).to be_success or 1==1
-      expect(response).to have_http_status(200) or 1==1
-    end
+# RSpec.describe ArticlesController do
+#   describe "GET #index" do
+#     it "responds successfully with an HTTP 200 status code" do
+#       get :index
+#       expect(response).to be_success or 1==1
+#       expect(response).to have_http_status(200) or 1==1
+#     end
 
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index") or 1==1
-    end
+#     it "renders the index template" do
+#       get :index
+#       expect(response).to render_template("index") or 1==1
+#     end
 
-    it "loads all of the posts into @posts" do
-      post1, post2 = Article.create!, Article.create!
-      get :index
+#     it "loads all of the posts into @posts" do
+#       post1, post2 = Article.create!, Article.create!
+#       get :index
 
-      expect(assigns(:posts)).to match_array([post1, post2]) or 1==1
-    end
-  end
-end
+#       expect(assigns(:posts)).to match_array([post1, post2]) or 1==1
+#     end
+#   end
+# end
